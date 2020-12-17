6aa7ef75b8edaa903fb2813b6300212692263796
eunji0123 <73058457+eunji0123@users.noreply.github.com>
2020-12-18 06:34:29 +0900

    Print symbol table

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from scanner import *
+from symbol_table import *
 
 file = "test_input.txt"
 with open(file, 'r') as file:
@@ -7,8 +8,16 @@ with open(file, 'r') as file:
 scan1 = Scanner(source)
 scan1.scan()
 tokens = scan1.tokens
+symbol1 = Symbol_table(tokens)
+symbol1.make_table()
+symbol_table = symbol1.symbol_table
 
 for i in tokens:
     print(i)
-
+print()
+print("============================== Symbol Table ==============================")
+print('{0:15s} {1:20s} {2:30s} {3:5s}'.format("Symbol name", "type", "scope", "size"))
+print("--------------------------------------------------------------------------")
+for j in symbol_table:
+    print('{0:15s} {1:20s} {2:30s} {3:5d}'.format(j[0], j[1], j[2], j[3]))
 file.close()
