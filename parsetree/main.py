0f9b583dc61152d9c0db500633a718ac5e6ce591
ywywyw1111111 <76167532+ywywyw1111111@users.noreply.github.com>
2020-12-20 16:55:19 +0900

    Delete main.py

--- parsetree/main.py
+++
@@ -1,6 +0,0 @@
-from tree import *
-
-reduce_log=['r2', 'r22', 'r20', 'r18', 'r13', 'r10', 'r22', 'r20', 'r18', 'r13', 'r9', 'r22', 'r20', 'r22', 'r19', 'r13', 'r9', 'r7', 'r0'] #parsing table에서 accept 나올 때까지 했던 reduce 연산
-
-tree=parsetree(reduce_log)
-root=tree.maketree()
